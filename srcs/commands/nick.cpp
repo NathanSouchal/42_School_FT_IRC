303460556dde113adce025f857ada2e22a04b3b7
Timo Nicolaux <tnicolau@z3r11p3.42lyon.fr>
2024-10-30 15:02:21 +0100

    updated user

--- srcs/commands/nick.cpp
+++ srcs/commands/nick.cpp
@@ -7,17 +7,12 @@ void	Server::nickname(const std::string& message, Client *client)
 	size_t		pos = message.find(" ");
 	std::string	nickname_sent;
 
-	if (pos == std::string::npos)
-		client->reply(ERR_NEEDMOREPARAMS(client->getNickname(), "NICK"));
-	else
-	{
-		nickname_sent = message.substr(pos + 1);
-		if (nickname_sent.size() > 9 || nickname_sent.find(",") != std::string::npos)
-			return client->reply(ERR_ERRONEUSNICKNAME(nickname_sent));
-		if (findNickName(nickname_sent))
-			return client->reply(ERR_NICKNAMEINUSE(nickname_sent));
-		client->setNick(nickname_sent);
-		if (!(client->getRealname().empty()) && !(client->getUsername().empty()))
-			client->setTrueRegistration();
-	}
+	nickname_sent = message.substr(pos + 1);
+	if (nickname_sent.size() > 9 || nickname_sent.find(",") != std::string::npos)
+		return client->reply(ERR_ERRONEUSNICKNAME(nickname_sent));
+	if (findNickName(nickname_sent))
+		return client->reply(ERR_NICKNAMEINUSE(nickname_sent));
+	client->setNick(nickname_sent);
+	if (!(client->getRealname().empty()) && !(client->getUsername().empty()))
+		client->setTrueRegistration();
 }
